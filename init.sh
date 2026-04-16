22d12013add0a39efdfdcafe207697a64d7058cb
Foahh <160589889+Foahh@users.noreply.github.com>
2026-04-17 03:22:38 +0800

    Add init script for submodule synchronization and updates

---
+++ init.sh
@@ -0,0 +1,11 @@
+git submodule sync external/fyp-power-measure
+git submodule sync external/stm32ai-modelzoo
+git submodule sync external/stm32ai-modelzoo-services
+git submodule sync external/ultralytics
+git submodule sync external/TinyissimoYOLO
+
+git submodule update --init --recursive --depth 1 external/fyp-power-measure
+git submodule update --init --recursive --depth 1 external/stm32ai-modelzoo
+git submodule update --init --recursive --depth 1 external/stm32ai-modelzoo-services
+git submodule update --init --recursive --depth 1 external/ultralytics
+git submodule update --init --recursive --depth 1 external/TinyissimoYOLO
